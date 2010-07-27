cdb2df6e850d85599208e6c0e2bd5ddca0e2c532
Mykola Rybak <mykola.rybak@gmail.com>
2010-07-27 22:03:14 +0300

    Сменил название приложения и язык

--- protected/config/main.php
+++ protected/config/main.php
@@ -7,7 +7,8 @@
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'My Web Application',
+	'name'=>'Школа системной соционики',
+	'sourceLanguage' => 'ru',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
