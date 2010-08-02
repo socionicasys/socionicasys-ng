05cca5158fac2b063d826b7f35272975940e9ad6
Mykola Rybak <mykola.rybak@gmail.com>
2010-08-02 12:56:20 +0300

    Дерево сайта, пробная версия

--- protected/config/main.php
+++ protected/config/main.php
@@ -77,7 +77,83 @@ return array(
 	// application-level parameters that can be accessed
 	// using Yii::app()->params['paramName']
 	'params'=>array(
-		// this is used in contact page
-		'adminEmail'=>'webmaster@example.com',
+		'pageTree' => array(
+			'items' => array(
+				array(
+					'label' => 'Главная',
+					'url' => array('static/view'),
+					'items' => array(
+						array(
+							'label' => 'История соционики',
+							'url' => array('static/view', 'path' => 'istorija-socioniki'),
+						),
+						array(
+							'label' => 'О Школе системной соционики',
+							'url' => array('static/view', 'path' => 'o-shkole'),
+						),
+						array(
+							'label' => 'Новости',
+							'url' => array('static/view', 'path' => 'novosti'),
+						),
+						array(
+							'label' => 'Контакты',
+							'url' => array('static/view', 'path' => 'kontaky'),
+						),
+					),
+				),
+				array(
+					'label' => 'Теория',
+					'url' => array('static/view', 'path' => 'teorija'),
+					'items' => array(
+						array(
+							'label' => 'Соционика для начинающих',
+							'url' => array('static/view', 'path' => 'teorija/socionika-dlja-nachinajushhih'),
+						),
+						array(
+							'label' => 'Вступление к системной соционике',
+							'url' => array('static/view', 'path' => 'teorija/vstuplenije'),
+						),
+						array(
+							'label' => 'Системный подход',
+							'url' => array('static/view', 'path' => 'teorija/sistemnyj-podhod'),
+							'items' => array(
+								array(
+									'label' => 'Система',
+									'url' => array('static/view', 'path' => 'teorija/sistemnyj-podhod/sistema'),
+								),
+								array(
+									'label' => 'Системные принципы',
+									'url' => array('static/view', 'path' => 'teorija/sistemnyj-podhod/sistemnye-principy'),
+								),
+							),
+						),
+						array(
+							'label' => 'Деление информации на аспекты',
+							'url' => array('static/view', 'path' => 'teorija/aspekty'),
+						),
+						array(
+							'label' => 'Модель ТИМa',
+							'url' => array('static/view', 'path' => 'teorija/model-tima'),
+						),
+					),
+				),
+				array(
+					'label' => 'Практика определения ТИМа',
+					'url' => array('static/view', 'path' => 'praktika'),
+					'items' => array(
+					),
+				),
+				array(
+					'label' => 'Статьи, доклады',
+					'url' => array('static/view', 'path' => 'statji-doklady'),
+					'items' => array(
+					),
+				),
+				array(
+					'label' => 'Форум',
+					'url' => '/forum',
+				),
+			),
+		),
 	),
 );
