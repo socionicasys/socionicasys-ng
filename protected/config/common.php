f0766b1d051c43f9f3d9699b5a130e60cb36ee53
Mykola Rybak <mykola.rybak@gmail.com>
2010-08-06 14:15:39 +0300

    Вынес настройки подключения к БД в локальный конфиг

--- protected/config/common.php
+++ protected/config/common.php
@@ -46,19 +46,6 @@ return array(
 				'/' => 'static/view',
 			),
 		),
-		'db'=>array(
-			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
-		),
-		// uncomment the following to use a MySQL database
-		/*
-		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=testdrive',
-			'emulatePrepare' => true,
-			'username' => 'root',
-			'password' => '',
-			'charset' => 'utf8',
-		),
-		*/
 		'errorHandler'=>array(
 			// use 'site/error' action to display errors
             'errorAction'=>'site/error',
