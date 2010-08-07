43ed50a7c1cee27b16abfc3c9e4e62f54c88ba44
Mykola Rybak <mykola.rybak@gmail.com>
2010-08-07 18:22:36 +0300

    Простейшее представление для списка новостей

---
+++ protected/views/news/list.php
@@ -0,0 +1,7 @@
+<?php
+$this->pageTitle='Новости | ' . Yii::app()->name;
+$this->widget('zii.widgets.CListView', array(
+	'dataProvider' => $dataProvider,
+	'itemView' => 'view',
+	'emptyText' => 'Новостей нет.'
+));
