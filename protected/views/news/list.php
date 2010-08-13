b56374f83d6b4df2c6c41c48b669bfa49a709086
Mykola Rybak <mykola.rybak@gmail.com>
2010-08-13 11:10:31 +0300

    Скрыть поле summary (элементы 1—5 из 10) в списке новостей

--- protected/views/news/list.php
+++ protected/views/news/list.php
@@ -3,5 +3,6 @@ $this->pageTitle='Новости | ' . Yii::app()->name;
 $this->widget('zii.widgets.CListView', array(
 	'dataProvider' => $dataProvider,
 	'itemView' => 'view',
-	'emptyText' => 'Новостей нет.'
+	'emptyText' => 'Новостей нет.',
+	'template' => '{items}\n{pager}',
 ));
