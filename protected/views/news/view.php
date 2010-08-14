48950e588d58bb34e5961ee93f6d6c6d78d359fa
Mykola Rybak <mykola.rybak@gmail.com>
2010-08-14 10:44:43 +0300

    Стили для новостей

--- protected/views/news/view.php
+++ protected/views/news/view.php
@@ -1,4 +1,4 @@
-<article>
+<article class="news-item">
 	<header>
 		<h1><?php echo CHtml::encode($data->title); ?></h1>
 		<p><time class="published updated" pubdate="" datetime="<?php echo date('Y-m-d', $data->post_time); ?>"><?php echo date('d.m.Y', $data->post_time); ?></time></p>
