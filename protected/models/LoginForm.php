6e8dc920e2aad6920f49bd990f2261c6abb3af80
Mykola Rybak <mykola.rybak@gmail.com>
2010-08-19 15:22:07 +0300

    Простейшая модель для формы логина

---
+++ protected/models/LoginForm.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ * Модель для формы логина.
+ */
+class LoginForm extends CFormModel
+{
+	public $username;
+	public $password;
+	public $rememberMe;
+	
+	private $_identity;
+	
+	public function rules()
+	{
+		return array(
+			array('username, password', 'required'),
+			array('rememberMe', 'boolean'),
+			array('password', 'authenticate', 'skipOnError' => true),
+		);
+	}
+	
+	public function attributeLabels()
+	{
+		return array(
+			'username' => 'Имя пользователя',
+			'password' => 'Пароль',
+			'rememberMe' => 'Запомнить меня на этом компьютере',
+		);
+	}
+	
+	public function authenticate($attribute = null, $params = null)
+	{
+		$this->_identity = new UserIdentity($this->username, $this->password);
+		if (!$this->_identity->authenticate())
+		{
+			if ($this->_identity->errorCode == UserIdentity::ERROR_USERNAME_INVALID)
+			{
+				$this->addError('username', 'Неверное имя пользователя');
+			}
+			else
+			{
+				$this->addError('password', 'Неверный пароль');
+			}
+		}
+	}
+	
+	public function login()
+	{
+		if ($this->_identity === null)
+		{
+			$this->authenticate();
+		}
+		if ($this->_identity->errorCode === UserIdentity::ERROR_NONE)
+		{
+			$duration = $this->rememberMe? 3600 * 24 * 30 : 0; // 30 дней
+			Yii::app()->user->login($this->_identity, $duration);
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+}
