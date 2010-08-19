43b7a7280469f1944dfa4cb7138747b8e7c1ce18
Mykola Rybak <mykola.rybak@gmail.com>
2010-08-19 06:47:43 +0300

    Функции проверки пароля в модели User

--- protected/models/User.php
+++ protected/models/User.php
@@ -91,4 +91,14 @@ class User extends CActiveRecord
 			'criteria'=>$criteria,
 		));
 	}
+	
+	public function validatePassword($password)
+	{
+		return $this->hashPassword($password, $this->salt) === $this->password;
+	}
+	
+	public function hashPassword($password, $salt)
+	{
+		return md5($salt . $password);
+	}
 }
