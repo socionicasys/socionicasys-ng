6a282855ab2d877d82c087b3afef4b32a48a2771
Mykola Rybak <mykola.rybak@gmail.com>
2010-08-22 10:16:26 +0300

    Надписи на странице с сообщением об ошибке на русском

--- protected/views/site/error.php
+++ protected/views/site/error.php
@@ -1,11 +1,11 @@
 <?php
-$this->pageTitle=Yii::app()->name . ' - Error';
+$this->pageTitle=Yii::app()->name;
 $this->breadcrumbs=array(
-	'Error',
+	'Ошибка',
 );
 ?>
 
-<h2>Error <?php echo $code; ?></h2>
+<h2>Ошибка <?php echo $code; ?></h2>
 
 <div class="error">
 <?php echo CHtml::encode($message); ?>
