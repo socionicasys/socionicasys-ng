3626c746ab1f10290bc90fdbeeb6b4c5231ecb37
Mykola Rybak <mykola.rybak@gmail.com>
2010-09-11 16:25:55 +0300

    Настройки консольного приложения. Подключен модуль миграций БД.

---
+++ protected/config/console.php
@@ -0,0 +1,22 @@
+<?php
+
+$localName = dirname(__FILE__) . '/console-local.php';
+
+if (file_exists($localName))
+{
+	$localConfig = require($localName);
+}
+else
+{
+	$localConfig = array();
+}
+
+return CMap::mergeArray($localConfig, array(
+	'basePath' => dirname(__FILE__) . DIRECTORY_SEPARATOR . '..',
+	'name' => 'socionicasys-ng',
+	'commandMap' => array(
+		'migrate' => array(
+			'class' => 'ext.yii-dbmigrations.CDbMigrationCommand',
+		),
+	),
+));
