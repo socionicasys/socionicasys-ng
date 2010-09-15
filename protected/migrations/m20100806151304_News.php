9cda834a1537b3e4ffadc9981b6d8ac76a64b256
Mykola Rybak <mykola.rybak@gmail.com>
2010-09-15 22:47:44 +0300

    Предыдущие изменения в БД в виде файлов миграций

---
+++ protected/migrations/m20100806151304_News.php
@@ -0,0 +1,19 @@
+<?php
+
+class m20100806151304_News extends CDbMigration
+{
+	public function up()
+	{
+		$newsTable = $this->newTable('{{news}}');
+		$newsTable->primary_key('id');
+		$newsTable->string('title', 'NOT NULL');
+		$newsTable->text('text', 'NOT NULL');
+		$newsTable->integer('post_time', 'NOT NULL');
+		$this->addTable($newsTable);
+	}
+
+	public function down()
+	{
+		$this->removeTable('{{news}}');
+	}
+}
