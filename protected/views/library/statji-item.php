331c19c751e8f90885f1693bb7d1f78e49a45e1f
Mykola Rybak <mykola.rybak@gmail.com>
2010-10-02 18:14:49 +0300

    Стили для оформления текста статей

--- protected/views/library/statji-item.php
+++ protected/views/library/statji-item.php
@@ -9,4 +9,15 @@ $breadcrumbs = array_merge($breadcrumbs, array(
 $this->setBreadcrumbs($breadcrumbs);
 $this->pageTitle = $model->title . ' | ' . Yii::app()->name;
 $this->renderItemLinks($model->type, $model->url);
+?>
+<header>
+	<hgroup>
+		<h1 class="article-author"><?php echo CHtml::encode($model->author); ?></h1>
+		<h1 class="article-title"><?php echo CHtml::encode($model->title); ?></h1>
+		<?php if (!empty($model->published)): ?>
+			<p class="published-info">Опубликовано в: <?php echo CHtml::encode($model->published); ?></p>
+		<?php endif; ?>
+	</hgroup>
+</header>
+<?php
 echo $model->text;
