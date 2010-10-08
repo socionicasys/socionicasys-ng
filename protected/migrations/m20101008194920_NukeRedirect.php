f432d3deaae824eeef2ab53892ae17172b8b6464
Mykola Rybak <mykola.rybak@gmail.com>
2010-10-08 21:04:06 +0300

    Таблица для перенаправления ссылок со старого сайта

---
+++ protected/migrations/m20101008194920_NukeRedirect.php
@@ -0,0 +1,21 @@
+<?php
+
+class m20101008194920_NukeRedirect extends CDbMigration
+{
+	public function up()
+	{
+		$redirectTable = $this->newTable('{{redirect}}');
+		$redirectTable->primary_key('id');
+		$redirectTable->string('group', 2);
+		$redirectTable->string('page', 2);
+		$redirectTable->string('model_class', 64);
+		$redirectTable->integer('model_id');
+		$redirectTable->unique('redirect.page', array('group', 'page'));
+		$this->addTable($redirectTable);
+	}
+	
+	public function down()
+	{
+		$this->removeTable('{{redirect}}');
+	}
+}
