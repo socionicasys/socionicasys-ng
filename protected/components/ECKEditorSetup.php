909ce87df2b3693fc58a4f4cca1c353e5745eef9
Mykola Rybak <mykola.rybak@gmail.com>
2010-10-09 19:56:00 +0300

    Исправил ошибку в установке настроек CKEditor-а

--- protected/components/ECKEditorSetup.php
+++ protected/components/ECKEditorSetup.php
@@ -22,7 +22,7 @@ class ECKEditorSetup extends ECKEditor
 				'Maximize', 'Source',
 			),
 		);
-		$this->options = array(
+		$options = array(
 			'toolbarCanCollapse' => false,
 			'stylesSet' => array(
 				array(
@@ -78,8 +78,9 @@ class ECKEditorSetup extends ECKEditor
 		if (isset(Yii::app()->params['enableFileManager'])
 			&& Yii::app()->params['enableFileManager'])
 		{
-			$this->options['filebrowserBrowseUrl'] = CHtml::normalizeUrl(array('site/browse'));
+			$options['filebrowserBrowseUrl'] = CHtml::normalizeUrl(array('site/browse'));
 		}
+		$this->options = $options;
 		$this->htmlOptions = array(
 			'rows' => 10,
 			'cols' => 60,
