7f125beceefaeb7f2edce5e10bf76503d359d947
Mykola Rybak <mykola.rybak@gmail.com>
2010-10-27 12:03:23 +0300

    Обработка ошибок фильтрации с помощью Browscap

--- protected/components/SpaceFixer.php
+++ protected/components/SpaceFixer.php
@@ -11,11 +11,18 @@ class SpaceFixer extends COutputProcessor
 	public function processOutput($output)
 	{
 		$cachedir = Yii::app()->getRuntimePath();
-		$browscap = new Browscap($cachedir);
-		$browser = $browscap->getBrowser();
-		if ($browser->Browser === 'Opera')
+		try
 		{
-			$output = str_replace(array('&thinsp;', ' '), ' ', $output);
+			$browscap = new Browscap($cachedir);
+			$browser = $browscap->getBrowser();
+			if ($browser->Browser === 'Opera')
+			{
+				$output = str_replace(array('&thinsp;', ' '), ' ', $output);
+			}
+		}
+		catch (Browscap_Exception $e)
+		{
+			// Если не удалось создать объект Browscap или опознать браузер, не применяем фильтрацию
 		}
 		parent::processOutput($output);
 	}
