be6ebdd39eda75dca2a80bff89b1b6c090ed106f
Mykola Rybak <mykola.rybak@gmail.com>
2010-11-26 18:35:56 +0200

    Переместил ссылки редактирования новости

--- protected/views/news/_item.php
+++ protected/views/news/_item.php
@@ -2,5 +2,6 @@
 	<h1><?php echo CHtml::encode($data->title); ?></h1>
 	<p><time class="published updated" pubdate="" datetime="<?php echo date('Y-m-d', $data->post_time); ?>"><?php echo date('d.m.Y', $data->post_time); ?></time></p>
 </header> 
-<?php echo $data->text; ?>
-<?php $this->renderDynamic('renderItemLinks', $data->id); ?>
+<?php
+$this->renderDynamic('renderItemLinks', $data->id);
+echo $data->text;
