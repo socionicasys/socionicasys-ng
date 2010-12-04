093810a8c927a0318898c440429b5ff10395d4ae
Mykola Rybak <mykola.rybak@gmail.com>
2010-12-04 13:08:02 +0200

    Добавил к цитате заголовок, убрал цвет текста

--- protected/components/views/random-quote.php
+++ protected/components/views/random-quote.php
@@ -1,7 +1,8 @@
-<div class="random-quote">
+<section class="random-quote">
+	<h1>Мысли человечества в аспектах</h1>
 	<blockquote class="hyphenate">
 		<?php echo $quote->text; ?>
 	</blockquote>
 	<p class="random-quote-author"><?php echo $quote->author; ?></p>
 	<p class="random-quote-note">(<?php echo $quote->note; ?>)</p>
-</div>
+</section>
