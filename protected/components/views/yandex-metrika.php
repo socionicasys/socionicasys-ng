849a3b8193bcd32db2f4cea17c91792f7b1831d3
Mykola Rybak <mykola.rybak@gmail.com>
2011-01-05 09:39:42 +0200

    Обновлен код счетчика Яндекс.Метрики

--- protected/components/views/yandex-metrika.php
+++ protected/components/views/yandex-metrika.php
@@ -1,15 +1,16 @@
 <!-- Yandex.Metrika -->
-<script type="text/javascript">
-window.yandex_metrika_callback = function() {
-    window.yaCounter<?php echo $id; ?> = new Ya.Metrika(<?php echo $id; ?>);
-};
-(function(){var i="DOMContentLoaded",g="onreadystatechange",c="doScroll",f="addEventListener",
-o="attachEvent",d="load",l=false,n=document,e=window,m=n.documentElement,h=l,j;
-function k(){if(!h){h=!h;j=n.createElement("script");j.type="text/javascript";
-j.src=((document.location.protocol=="https:")?"https:":"http:")+"//mc.yandex.ru/metrika/watch.js";j.setAttribute("async","true");
-m.firstChild.appendChild(j)}}if(n[f]){function b(){n.removeEventListener(i,b,l);k()}n[f](i,b,l);
-n[f](d,k,l)}else{if(n[o]){n[o](g,a);function a(){if(h){return}try{m[c]("left")}catch(p){setTimeout(a,0);
-return}k()}if(m[c]&&e==e.top){a()}n[o]("on"+d,k)}}})();
-</script>
-<noscript><img src="//mc.yandex.ru/watch/<?php echo $id; ?>" style="position:absolute" alt="" /></noscript>
+<div style="display:none;"><script type="text/javascript">
+(function(w, c) {
+    (w[c] = w[c] || []).push(function() {
+        try {
+            w.yaCounter<?php echo $id; ?> = new Ya.Metrika(<?php echo $id; ?>);
+             yaCounter<?php echo $id; ?>.clickmap(true);
+             yaCounter<?php echo $id; ?>.trackLinks(true);
+        
+        } catch(e) {}
+    });
+})(window, 'yandex_metrika_callbacks');
+</script></div>
+<script src="//mc.yandex.ru/metrika/watch.js" type="text/javascript" defer="defer"></script>
+<noscript><div style="position:absolute"><img src="//mc.yandex.ru/watch/<?php echo $id; ?>" alt="" /></div></noscript>
 <!-- /Yandex.Metrika -->
