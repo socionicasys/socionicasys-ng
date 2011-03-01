96763813e0cd80580910eb350a3757f0376ee2f4
Mykola Rybak <mykola.rybak@gmail.com>
2011-03-01 11:18:32 +0200

    Отображение ссылок в разных форматах в форме редактирования протокола и в таблице протоколов

--- protected/views/protocol/list.php
+++ protected/views/protocol/list.php
@@ -1,7 +1,8 @@
 <h1>Протоколы идентификации ТИМа</h1>
 
 <p>Протоколы, созданные после июля 2010 года, были сгенерированны в программе
-<a href="/praktika/programma-informacionnyj-analiz">Информационный анализ</a>.
+<a href="/praktika/programma-informacionnyj-analiz">Информационный анализ</a> (ИА).
+Протоколы, созданные раньше, доступны в формате MS Word, а также переведены в формат ИА.</p>
 
 <?php if ($canCreate) : ?>
 <p><a href="<?php echo $this->createUrl('create'); ?>">Добавить протокол</a></p>
@@ -10,7 +11,7 @@
 <?php
 $this->pageTitle='Протоколы | ' . Yii::app()->name;
 
-$buttonTemplate = '{view}';
+$buttonTemplate = '';
 if ($canEdit)
 {
 	$buttonTemplate .= ' {update}';
@@ -19,34 +20,59 @@ if ($canDelete)
 {
 	$buttonTemplate .= ' {delete}';
 }
+$columns = array(
+	array(
+		'name' => 'name',
+		'type' => 'raw',
+		'value' => 'CHtml::encode($data->name) . (empty($data->comment) ? "" :
+			CHtml::link("*", "", array("title" => $data->comment)))',
+	),
+	'tim',
+	'date',
+	array(
+		'class' => 'CButtonColumn',
+		'template' => '{view}',
+		'buttons' => array(
+			'view' => array(
+				'label' => 'Просмотреть',
+				'url' => '$data->url',
+				'visible' => '!empty($data->url)',
+			),
+		),
+		'header' => 'ИА',
+		'headerHtmlOptions' => array(
+			'title' => 'Ссылки на протокол в формате ИА (программа «Информационный анализ»)',
+		),
+	),
+	array(
+		'class' => 'CButtonColumn',
+		'template' => '{view}',
+		'buttons' => array(
+			'view' => array(
+				'label' => 'Скачать',
+				'url' => '$data->legacy_url',
+				'visible' => '!empty($data->legacy_url)',
+			),
+		),
+		'header' => 'Word',
+		'headerHtmlOptions' => array(
+			'title' => 'Ссылки на протокол в формате Microsoft Word',
+		),
+	),
+);
+if ($buttonTemplate !== '')
+{
+	$columns[] = array(
+		'class' => 'CButtonColumn',
+		'template' => $buttonTemplate,
+		'updateButtonUrl' => 'Yii::app()->controller->createUrl("edit", array("id" => $data->id))',
+	);
+}
+
 $this->widget('zii.widgets.grid.CGridView', array(
 	'id' => 'protocol-table',
 	'dataProvider' => $dataProvider,
 	'enablePagination' => false,
 	'template' => '{items}',
-	'columns' => array(
-		array(
-			'name' => 'name',
-			'type' => 'raw',
-			'value' => 'CHtml::encode($data->name) . (empty($data->comment) ? "" :
-				CHtml::link("*", "", array("title" => $data->comment)))',
-		),
-		'tim',
-		'date',
-		array(
-			'name' => 'ia',
-			'value' => '$data->ia ? "Да" : "Нет"',
-			'header' => 'ИА',
-			'headerHtmlOptions' => array(
-				'title' => 'Сохранен ли протокол в формате ИА (программа «Информационный анализ»)'
-			),
-		),
-		array(
-			'class' => 'CButtonColumn',
-			'template' => $buttonTemplate,
-			'viewButtonUrl' => '$data->url',
-			'viewButtonLabel' => 'Скачать',
-			'updateButtonUrl' => 'Yii::app()->controller->createUrl("edit", array("id" => $data->id))',
-		),
-	),
+	'columns' => $columns,
 ));
