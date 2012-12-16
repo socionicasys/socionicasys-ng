5efa690567686fb91efd33c02a563642c0cc99ed
Mykola Rybak <mykola.rybak@gmail.com>
2012-12-16 12:56:05 +0200

    Асинхронный код Yandex.Metrika

--- protected/components/views/yandex-metrika.php
+++ protected/components/views/yandex-metrika.php
@@ -1,16 +1,25 @@
 <!-- Yandex.Metrika -->
-<div style="display:none;"><script type="text/javascript">
-(function(w, c) {
-    (w[c] = w[c] || []).push(function() {
-        try {
-            w.yaCounter<?php echo $id; ?> = new Ya.Metrika(<?php echo $id; ?>);
-             yaCounter<?php echo $id; ?>.clickmap(true);
-             yaCounter<?php echo $id; ?>.trackLinks(true);
-        
-        } catch(e) {}
-    });
-})(window, 'yandex_metrika_callbacks');
-</script></div>
-<script src="//mc.yandex.ru/metrika/watch.js" type="text/javascript" defer="defer"></script>
-<noscript><div style="position:absolute"><img src="//mc.yandex.ru/watch/<?php echo $id; ?>" alt="" /></div></noscript>
+<script type="text/javascript">
+    (function (d, w, c) {
+        (w[c] = w[c] || []).push(function() {
+            try {
+                w.yaCounter<?php echo $id; ?> = new Ya.Metrika({id:<?php echo $id; ?>,
+                    clickmap:true,
+                    trackLinks:true});
+            } catch(e) { }
+        });
+
+        var n = d.getElementsByTagName("script")[0],
+                s = d.createElement("script"),
+                f = function () { n.parentNode.insertBefore(s, n); };
+        s.type = "text/javascript";
+        s.async = true;
+        s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js";
+
+        if (w.opera == "[object Opera]") {
+            d.addEventListener("DOMContentLoaded", f, false);
+        } else { f(); }
+    })(document, window, "yandex_metrika_callbacks");
+</script>
+<noscript><div><img src="//mc.yandex.ru/watch/1739449" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
 <!-- /Yandex.Metrika -->
