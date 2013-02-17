aabf8521b0cfa1aa3de735d22997fb776e4a8594
Mykola Rybak <mykola.rybak@gmail.com>
2013-02-17 22:38:57 +0200

    WIP: Google Translate

--- protected/views/layouts/article-wide.php
+++ protected/views/layouts/article-wide.php
@@ -1,5 +1,19 @@
 <?php $this->beginContent('//layouts/wide'); ?>
 <article <?php echo $this->contentAttr; ?>>
+    <div id="google_translate_element"></div>
+    <script type="text/javascript">
+        function googleTranslateElementInit() {
+            new google.translate.TranslateElement({
+                        pageLanguage: 'ru',
+                        includedLanguages: 'en',
+                        layout: google.translate.TranslateElement.InlineLayout.SIMPLE,
+                        autoDisplay: false,
+                        gaTrack: true,
+                        gaId: 'UA-8823766-1'}, 'google_translate_element'
+            );
+        }
+    </script>
+    <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
 	<?php echo $content; ?>
 </article>
 <?php $this->endContent();
