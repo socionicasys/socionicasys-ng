1b3b1808f6a976650e353f9a5f029a9a718b0ac7
Mykola Rybak <mykola.rybak@gmail.com>
2013-02-17 23:00:44 +0200

    WIP: Google Translate

--- protected/views/layouts/article.php
+++ protected/views/layouts/article.php
@@ -1,19 +1,5 @@
 <?php $this->beginContent('//layouts/full'); ?>
 <article <?php echo $this->contentAttr; ?>>
-    <div id="google_translate_element"></div>
-    <script type="text/javascript">
-        function googleTranslateElementInit() {
-            new google.translate.TranslateElement({
-                pageLanguage: 'ru',
-                includedLanguages: 'en',
-                layout: google.translate.TranslateElement.InlineLayout.SIMPLE,
-                autoDisplay: false,
-                gaTrack: true,
-                gaId: 'UA-8823766-1'}, 'google_translate_element'
-            );
-        }
-    </script>
-    <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
 	<?php echo $content; ?>
 </article>
 <?php $this->endContent();
--- protected/views/layouts/common.php
+++ protected/views/layouts/common.php
@@ -24,6 +24,20 @@ $base = Yii::app()->request->baseUrl;
 <body id="body">
 	<div id="wrap">
 		<header id="main-header">
+            <div id="google_translate_element"></div>
+            <script type="text/javascript">
+                function googleTranslateElementInit() {
+                    new google.translate.TranslateElement({
+                                pageLanguage: 'ru',
+                                includedLanguages: 'en',
+                                layout: google.translate.TranslateElement.InlineLayout.SIMPLE,
+                                autoDisplay: false,
+                                gaTrack: true,
+                                gaId: 'UA-8823766-1'}, 'google_translate_element'
+                    );
+                }
+            </script>
+            <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
 			<?php $this->renderDynamic('renderHeaderLinks'); ?>
 			<a id="logo-link" href="<?php echo Yii::app()->homeUrl; ?>">
 				<h1>Школа системной соционики</h1>
